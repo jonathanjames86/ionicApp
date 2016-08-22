6c2d8a2e11c32986118bf88c0b41bf7ffd1ed9f4
James Putnam <jonathan.putnam86@gmail.com>
2016-08-22 17:13:52 -0600

    working on map API

--- app/pages/local-db/local-db.ts
+++ app/pages/local-db/local-db.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
 import { Animation } from '../animation/animation'
-import { Camera } from 'ionic-native';
+import { Camera, Geolocation } from 'ionic-native';
 
 
+declare var google;
 
 
 @Component({
@@ -11,11 +12,52 @@ import { Camera } from 'ionic-native';
 
 })
 export class LocalDb {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   animation = Animation;
   public base64Image: string;
   constructor(private navCtrl: NavController) {
 
   }
+//
+//   ionViewLoaded(){
+//     this.loadMap();
+//   }
+//
+//   ///Using Geolocation to find current Position
+//   loadMap(){
+//     Geolocation.getCurrentPosition().then((position) => {
+//       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//       let mapOptions = {
+//         center: latLng,
+//         zoom: 15,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//       }
+//       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+//     }, (err) => {
+//       console.log(err);
+//     });
+//   }
+//   addMarker(){
+//   let marker = new google.maps.Marker({
+//     map: this.map,
+//     animation: google.maps.Animation.DROP,
+//     position: this.map.getCenter()
+//   });
+//   let content = "<h4>Information!</h4>";
+//   this.addInfoWindow(marker, content);
+//
+// }
+//   addInfoWindow(marker, content){
+//     let infoWindow = new google.maps.InfoWindow({
+//       content: content
+//     });
+//     google.maps.event.addListener(marker, 'click', () => {
+//       infoWindow.open(this.map, marker);
+//     });
+//   }
   takePicture(){
     Camera.getPicture({
         destinationType: Camera.DestinationType.DATA_URL,
